36fa3fbb7e435d363a0491aa7265b4c684fba2b3
vanel <njikekamgnia@gmail.com>
2015-05-16 16:41:39 +0200

    20150516

--- com.kaassa.business/src/com/kaassa/business/views/HomePage.java
+++ com.kaassa.business/src/com/kaassa/business/views/HomePage.java
@@ -11,6 +11,7 @@ import com.kaassa.business.models.Company;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -19,13 +20,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class HomePage extends Activity implements OnClickListener 
 {
 	Button search_company;
 	Button search_executive;
+	Dialog dialog;
 	
 	CallKaassaBusinessWS callKaassaWS = new CallKaassaBusinessWS();
 	ManageViewTransitions manageView = new ManageViewTransitions();
@@ -85,16 +90,9 @@ public class HomePage extends Activity implements OnClickListener
 
 	    	    return true;
 			case R.id.action_leaveusmessage:
-	           	
-              //Add the following lines
-            	Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("message/rfc822");
-                i.putExtra(Intent.EXTRA_EMAIL, "test@kassa.com");
-            	
-             // Verify that the intent will resolve to an activity
-                if (i.resolveActivity(getPackageManager()) != null) {
-                    startActivity(Intent.createChooser(i, "Choose an Email client :"));
-                }
+	           	// Display contact us Dialog
+				displayContactusDialog();
+				
 				return true;
 		    default:
 		        return super.onOptionsItemSelected(item);
@@ -127,7 +125,76 @@ public class HomePage extends Activity implements OnClickListener
 			// Call the Transition method From the current view to "Executives List View"
 			manageView.HomepageToExecutivelist(v.getContext());
 		}
+
 	}
+	
+	private void displayContactusDialog() 
+	{
+		// TODO Auto-generated method stub
+		//Display a dialog to filter the hotellist	
+		dialog = new Dialog(this);
+		
+        // Include dialog.xml file
+        dialog.setContentView(R.layout.dialog_companylist_filter);
+        
+        // Set dialog title
+        dialog.setTitle(R.string.menu_homepage_contactus);
+        
+        //Get Message Items
+        EditText name_edittext = (EditText)dialog.findViewById(R.id.contact_us_name_edittext);
+        EditText email_edittext = (EditText)dialog.findViewById(R.id.contact_us_email_edittext);
+        EditText subject_edittext = (EditText)dialog.findViewById(R.id.contact_us_subject_edittext);
+        EditText message_edittext = (EditText)dialog.findViewById(R.id.contact_us_message_edittext);
+        Button home_button = (Button)dialog.findViewById(R.id.contact_us_home_button);
+        Button send_button = (Button)dialog.findViewById(R.id.contact_us_send_button);
+        
+        // Display the dialog filter
+        dialog.show();        
+        
+
+        OnClickListener dialogContactus = new OnClickListener() {
+  	       @Override
+  	       public void onClick(View v) 
+  	       {
+  	         // TODO Auto-generated method stub
+  	   		switch(v.getId())
+  			{
+  				case R.id.contact_us_home_button:	
+  					closeDialog();
+  					break;
+  				
+  				case R.id.contact_us_send_button:
+  					leaveusamessage();
+  			        break;
+  			}
+  	       }
+         };
+         
+		// if decline button is clicked, close the custom dialog
+        home_button.setOnClickListener(dialogContactus);
+        send_button.setOnClickListener(dialogContactus);        
+        
+	}
+
+	private OnClickListener leaveusamessage() {
+		// TODO Auto-generated method stub
+		
+        // Close dialog
+		dialog.dismiss();
+        
+		return null;
+	}
+
+	private OnClickListener closeDialog() {
+		// TODO Auto-generated method stub
+		
+        // Close dialog
+		dialog.dismiss();
+		return null;
+	}
+
+	
+	
 
 			
 }
